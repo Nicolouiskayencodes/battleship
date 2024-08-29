12461ce5cdb8c6f7380e8f42194054e0a3b52cf9
Nicolas Louis-Kayen <nicolouiskayencodes@gmail.com>
2024-08-28 21:56:18 -0700

    Revert cpu logic and make modals show up when cpu sinks or wins

--- src/render-board.js
+++ src/render-board.js
@@ -1,133 +1,151 @@
-export default function render(player, player2, activePlayer = 1) {
-  if (player2.type === 'human'){
-  let array = player2.board;
-  let game = document.querySelector(".game");
-  let board = document.createElement("div");
-  let dialog = document.querySelector("#sunk");
-  board.classList.add("gameboard");
-  array.getBoard().forEach((element) => {
-    let row = document.createElement("div");
-    row.classList.add("row");
-    element.forEach((cell) => {
-      let space = document.createElement("div");
-      space.classList.add("space");
-      space.addEventListener("click", () => {
-        if (activePlayer === 1) {
+export default function render(
+  player,
+  player2,
+  activePlayer = 1
+) {
+  if (player2.type === "human") {
+    let array = player2.board;
+    let game = document.querySelector(".game");
+    let board = document.createElement("div");
+    let dialog = document.querySelector("#sunk");
+    board.classList.add("gameboard");
+    array.getBoard().forEach((element) => {
+      let row = document.createElement("div");
+      row.classList.add("row");
+      element.forEach((cell) => {
+        let space = document.createElement("div");
+        space.classList.add("space");
+        space.addEventListener("click", () => {
+          if (activePlayer === 1) {
+            let attack = array.receiveAttack([
+              array.getBoard().indexOf(element),
+              element.indexOf(cell),
+            ]);
+            game.textContent = "";
+            if (attack !== "Already shot") {
+              render(player, player2, 2);
+            } else if (attack === "Already shot") {
+              render(player, player2, 1);
+            }
+            if (attack === "sunk") {
+              dialog.textContent = "Player 2's ship sunk!";
+              let button = document.createElement("button");
+              button.textContent = "Continue";
+              button.addEventListener("click", () => {
+                dialog.close();
+              });
+              dialog.appendChild(button);
+              dialog.showModal();
+            }
+            if (attack === true) {
+              document.querySelector("#gameover").showModal();
+            }
+          }
+        });
+        if (cell.shot === true && cell.target !== null) {
+          space.classList.add("hit");
+        } else if (cell.shot === true && cell.target === null) {
+          space.classList.add("miss");
+        }
+        row.appendChild(space);
+      });
+      board.appendChild(row);
+    });
+    game.appendChild(board);
+    let array2 = player.board;
+    let board2 = document.createElement("div");
+    board2.classList.add("gameboard");
+    array2.getBoard().forEach((element) => {
+      let row = document.createElement("div");
+      row.classList.add("row");
+      element.forEach((cell) => {
+        let space = document.createElement("div");
+        space.classList.add("space");
+        space.addEventListener("click", () => {
+          if (activePlayer === 2) {
+            let attack = array2.receiveAttack([
+              array2.getBoard().indexOf(element),
+              element.indexOf(cell),
+            ]);
+            game.textContent = "";
+            if (attack !== "Already shot") {
+              render(player, player2, 1);
+            } else if (attack === "Already shot") {
+              render(player, player2, 2);
+            }
+            if (attack === "sunk") {
+              dialog.textContent = "Player 1's ship sunk!";
+              let button = document.createElement("button");
+              button.textContent = "Continue";
+              button.addEventListener("click", () => {
+                dialog.close();
+              });
+              dialog.appendChild(button);
+              dialog.showModal();
+            }
+            if (attack === true) {
+              document.querySelector("#gameover").showModal();
+            }
+          }
+        });
+        if (cell.shot === true && cell.target !== null) {
+          space.classList.add("hit");
+        } else if (cell.shot === true && cell.target === null) {
+          space.classList.add("miss");
+        }
+        row.appendChild(space);
+      });
+      board2.appendChild(row);
+    });
+    game.appendChild(board2);
+  } else if (player2.type === "computer") {
+    let array = player2.board;
+    let game = document.querySelector(".game");
+    let board = document.createElement("div");
+    let dialog = document.querySelector("#sunk");
+    board.classList.add("gameboard");
+    array.getBoard().forEach((element) => {
+      let row = document.createElement("div");
+      row.classList.add("row");
+      element.forEach((cell) => {
+        let space = document.createElement("div");
+        space.classList.add("space");
+        space.addEventListener("click", () => {
           let attack = array.receiveAttack([
             array.getBoard().indexOf(element),
             element.indexOf(cell),
           ]);
           game.textContent = "";
           if (attack !== "Already shot") {
-            render(player, player2, 2);
-          } else if (attack === "Already shot") {
-            render(player, player2, 1);
-          }
-          if (attack === "sunk") {
-            dialog.textContent = "Ship sunk!";
-            let button = document.createElement("button");
-            button.textContent = "Continue";
-            button.addEventListener("click", () => {
-              dialog.close();
-            });
-            dialog.appendChild(button);
-            dialog.showModal();
-          }
-          if (attack === true) {
-            document.querySelector('#gameover').showModal();
-          }
-        }
-      });
-      if (cell.shot === true && cell.target !== null) {
-        space.classList.add("hit");
-      } else if (cell.shot === true && cell.target === null) {
-        space.classList.add("miss");
-      }
-      row.appendChild(space);
-    });
-    board.appendChild(row);
-  });
-  game.appendChild(board);
-  let array2 = player.board;
-  let board2 = document.createElement("div");
-  board2.classList.add("gameboard");
-  array2.getBoard().forEach((element) => {
-    let row = document.createElement("div");
-    row.classList.add("row");
-    element.forEach((cell) => {
-      let space = document.createElement("div");
-      space.classList.add("space");
-      space.addEventListener("click", () => {
-        if (activePlayer === 2) {
-          let attack = array2.receiveAttack([
-            array2.getBoard().indexOf(element),
-            element.indexOf(cell),
-          ]);
-          game.textContent = "";
-          if (attack !== "Already shot") {
-            render(player, player2, 1);
-          } else if (attack === "Already shot") {
-            render(player, player2, 2);
-          }
-          if (attack === "sunk") {
-            dialog.textContent = "Ship sunk!";
-            let button = document.createElement("button");
-            button.textContent = "Continue";
-            button.addEventListener("click", () => {
-              dialog.close();
-            });
-            dialog.appendChild(button);
-            dialog.showModal();
-          }
-          if (attack === true) {
-            document.querySelector('#gameover').showModal();
-          }
-        }
-      });
-      if (cell.shot === true && cell.target !== null) {
-        space.classList.add("hit");
-      } else if (cell.shot === true && cell.target === null) {
-        space.classList.add("miss");
-      }
-      row.appendChild(space);
-    });
-    board2.appendChild(row);
-  });
-  game.appendChild(board2);
-} else if (player2.type === 'computer'){
-  let array = player2.board;
-  let game = document.querySelector(".game");
-  let board = document.createElement("div");
-  let dialog = document.querySelector("#sunk");
-  board.classList.add("gameboard");
-  array.getBoard().forEach((element) => {
-    let row = document.createElement("div");
-    row.classList.add("row");
-    element.forEach((cell) => {
-      let space = document.createElement("div");
-      space.classList.add("space");
-      space.addEventListener("click", () => {
-          let attack = array.receiveAttack([
-            array.getBoard().indexOf(element),
-            element.indexOf(cell),
-          ]);
-          game.textContent = "";
-          if (attack !== "Already shot") {
-            function computerAttack(){
-              let x = Math.floor(Math.random()*10);
-              let y = Math.floor(Math.random()*10);
-              let cpuAttack = player.board.receiveAttack([x,y]);
-              if (cpuAttack === 'Already shot'){
+            function computerAttack(extend=0) {
+              let x = Math.floor(Math.random() * 10);
+              let y = Math.floor(Math.random() * 10);
+              let cpuAttack = player.board.receiveAttack([x, y]);
+              if (cpuAttack === "Already shot") {
                 computerAttack();
               }
+              if (cpuAttack === "sunk") {
+                dialog.textContent = "Player 1's ship sunk!";
+                let button = document.createElement("button");
+                button.textContent = "Continue";
+                button.addEventListener("click", () => {
+                  dialog.close();
+                });
+                dialog.appendChild(button);
+                dialog.showModal();
+              }
+              if (cpuAttack === true) {
+                document.querySelector("#gameover").showModal();
+              }
             }
-            computerAttack();
-            render(player, player2, 1);
+            let isHit = computerAttack();
+            render(player, player2, 1, isHit);
           } else if (attack === "Already shot") {
-            render(player, player2, 1);
+              render(player, player2, 1)
+            
           }
           if (attack === "sunk") {
-            dialog.textContent = "Ship sunk!";
+            dialog.textContent = "Player 2's ship sunk!";
             let button = document.createElement("button");
             button.textContent = "Continue";
             button.addEventListener("click", () => {
@@ -137,37 +155,37 @@ export default function render(player, player2, activePlayer = 1) {
             dialog.showModal();
           }
           if (attack === true) {
-            document.querySelector('#gameover').showModal();
+            document.querySelector("#gameover").showModal();
           }
+        });
+        if (cell.shot === true && cell.target !== null) {
+          space.classList.add("hit");
+        } else if (cell.shot === true && cell.target === null) {
+          space.classList.add("miss");
+        }
+        row.appendChild(space);
       });
-      if (cell.shot === true && cell.target !== null) {
-        space.classList.add("hit");
-      } else if (cell.shot === true && cell.target === null) {
-        space.classList.add("miss");
-      }
-      row.appendChild(space);
+      board.appendChild(row);
     });
-    board.appendChild(row);
-  });
-  game.appendChild(board);
-  let array2 = player.board;
-  let board2 = document.createElement("div");
-  board2.classList.add("gameboard");
-  array2.getBoard().forEach((element) => {
-    let row = document.createElement("div");
-    row.classList.add("row");
-    element.forEach((cell) => {
-      let space = document.createElement("div");
-      space.classList.add("space");
-      if (cell.shot === true && cell.target !== null) {
-        space.classList.add("hit");
-      } else if (cell.shot === true && cell.target === null) {
-        space.classList.add("miss");
-      }
-      row.appendChild(space);
+    game.appendChild(board);
+    let array2 = player.board;
+    let board2 = document.createElement("div");
+    board2.classList.add("gameboard");
+    array2.getBoard().forEach((element) => {
+      let row = document.createElement("div");
+      row.classList.add("row");
+      element.forEach((cell) => {
+        let space = document.createElement("div");
+        space.classList.add("space");
+        if (cell.shot === true && cell.target !== null) {
+          space.classList.add("hit");
+        } else if (cell.shot === true && cell.target === null) {
+          space.classList.add("miss");
+        }
+        row.appendChild(space);
+      });
+      board2.appendChild(row);
     });
-    board2.appendChild(row);
-  });
-  game.appendChild(board2);
-}
+    game.appendChild(board2);
+  }
 }
--- src/ui.js
+++ src/ui.js
@@ -14,24 +14,24 @@ let player2 = undefined;
 onePlayer.addEventListener('click', ()=>{
   player2 = Player('computer');
   placeShips();
-  document.querySelector('.game').textContent = ''
+  document.querySelector('.game').textContent = '';
   render(player1, player2);
   players.close();
 })
 twoPlayer.addEventListener('click', ()=>{
   player2 = Player('human');
   placeShips();
-  document.querySelector('.game').textContent = ''
+  document.querySelector('.game').textContent = '';
   render(player1, player2);
   players.close();
 })
 
 function placeShips(){
 player1.board.placeShip([0,0], 'right', 5);
-// human.board.placeShip([1,1], 'down', 4);
-// human.board.placeShip([3,1], 'right', 3);
-// human.board.placeShip([3,6], 'right', 3);
-// human.board.placeShip([6,1], 'right', 3);
+player1.board.placeShip([1,1], 'down', 4);
+player1.board.placeShip([3,1], 'right', 3);
+player1.board.placeShip([3,6], 'right', 3);
+player1.board.placeShip([6,1], 'right', 3);
 
 player2.board.placeShip([0,0], 'right', 5);
 player2.board.placeShip([1,1], 'down', 4);
