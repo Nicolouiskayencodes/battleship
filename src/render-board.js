3453b667d552a288ca020e2abfb535d377b59be8
Nicolas Louis-Kayen <nicolouiskayencodes@gmail.com>
2024-08-28 22:46:57 -0700

    Create semi-smart logic for the computer player to follow

--- src/render-board.js
+++ src/render-board.js
@@ -1,8 +1,4 @@
-export default function render(
-  player,
-  player2,
-  activePlayer = 1
-) {
+export default function render(player, player2, activePlayer = 1) {
   if (player2.type === "human") {
     let array = player2.board;
     let game = document.querySelector(".game");
@@ -117,9 +113,53 @@ export default function render(
           ]);
           game.textContent = "";
           if (attack !== "Already shot") {
-            function computerAttack(extend=0) {
+            function computerAttack() {
               let x = Math.floor(Math.random() * 10);
               let y = Math.floor(Math.random() * 10);
+              let board = player.board.getBoard();
+              function followUp() {
+                for (let i = 0; i < 10; i++) {
+                  for (let j = 0; j < 10; j++) {
+                    if (board[i][j].target !== null) {
+                      if (
+                        board[i][j].shot === true &&
+                        board[i][j].target.isSunk() === false
+                      ) {
+                        if (i + 1 < 9) {
+                          if (board[i + 1][j].shot !== true) {
+                            x = i + 1;
+                            y = j;
+                            return;
+                          }
+                        } 
+                        if (j + 1 < 9) {
+                          if (board[i][j + 1].shot !== true) {
+                            x = i;
+                            y = j + 1;
+                            return;
+                          }
+                        } 
+                        if (i - 1 > 0) {
+                          if (board[i - 1][j].shot !== true) {
+                            x = i - 1;
+                            y = j;
+                            return;
+                          }
+                        } 
+                        if (j - 1 > 0) {
+                          if (board[i][j - 1].shot !== true) {
+                            x = i;
+                            y = j - 1;
+                            return;
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+              followUp();
+
               let cpuAttack = player.board.receiveAttack([x, y]);
               if (cpuAttack === "Already shot") {
                 computerAttack();
@@ -141,8 +181,7 @@ export default function render(
             let isHit = computerAttack();
             render(player, player2, 1, isHit);
           } else if (attack === "Already shot") {
-              render(player, player2, 1)
-            
+            render(player, player2, 1);
           }
           if (attack === "sunk") {
             dialog.textContent = "Player 2's ship sunk!";
